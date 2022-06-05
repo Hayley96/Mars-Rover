f8b742f6260d56c53e6a8e48c853542551c1cbfe
Hayley96 <h.jonas@outlook.com>
2022-06-05 21:39:57 +0100

    Updated GIF fixed arrow not upating on icons

--- MarsRover/MissionManager/MoveCommands.cs
+++ MarsRover/MissionManager/MoveCommands.cs
@@ -5,23 +5,25 @@ public class MoveCommands
     {
         foreach (char command in movecommands)
         {
-            if (command.ToString().Equals("L"))
-                vehicle?.TurnLeft(vehicle.Direction.ToString());
-            if (command.ToString().Equals("R"))
-                vehicle?.TurnRight(vehicle.Direction.ToString());
-            if (command.ToString().Equals("M"))
+            if (command.ToString().Equals("L")) 
             {
+                vehicle?.TurnLeft(vehicle.Direction.ToString());
+                UpdateVehicleOnGrid.Update(vehicle, plateau);
+            }
+
+            if (command.ToString().Equals("R"))
+            {
+                vehicle?.TurnRight(vehicle.Direction.ToString());
+                UpdateVehicleOnGrid.Update(vehicle, plateau);
+            }
+            if (command.ToString().Equals("M"))
                 if(ValidationVehicleBoundary.IsOutOfPlateauBounds(vehicle, plateau))
-                {
                     if (ValidationVehicleCollision.MovementCollisionCheck(vehicle, plateau))
                     {
                         vehicle?.MoveForward(vehicle.Direction.ToString());
                         UpdateVehicleOnGrid.Update(vehicle, plateau);
                         plateau?.Draw(plateau.PlateauSizeX, plateau.PlateauSizeY, plateau.Grid);
                     }
-                }
-          
-            }
         }
     }
 }
